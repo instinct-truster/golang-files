317f9a8dc34ba217b2d3343dc9f95b72a77f36cb
Lance Turbes <instinct_truster@protonmail.com>
2024-08-13 14:11:07 -0700

    connected peer :3000

---
+++ p2p/transport.go
@@ -0,0 +1,9 @@
+package p2p
+
+// Peer is an interface that represents the remote node.
+type Peer interface{}
+
+// Transport is anything that handles the communitaction between the nodes in the network. This can be of the form (TCP, UDP, websockets, ...)
+type Transport interface {
+	ListenAndAccept() error
+}
